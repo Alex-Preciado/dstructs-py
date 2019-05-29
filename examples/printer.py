73e1a3a57954fc15dd77dc6ec3d716ff1dbdfdc1
Alex Preciado <alexpreciado1@gmail.com>
2019-05-29 02:07:31 -0400

    Fix dequeue method in printer example

--- examples/printer.py
+++ examples/printer.py
@@ -9,20 +9,7 @@ class PrinterQueue:
 		self.items.insert(0, item)
 		
 	def dequeue(self):
-		if self.items:
-			self.items.pop()
-		else:
-			return None
-		
-	def peek(self):
-		if self.items:
-			return self.items[-1]
-		else:
-			return None
-		
-	def size(self):
-		return len(self.items)
-		
+		return self.items.pop()
 		
 	def is_empty(self):
 		return self.items == []
@@ -38,32 +25,27 @@ class Job:
 			self.pages -= 1
 	
 	def check_complete(self):
-		if pages == 0:
+		if self.pages == 0:
 			return True
-		else:
-			return False
+		return False
 
 
 class Printer:
-	
+
 	def __init__(self):
 		self.current_job = None
-		
-	def get_job(self, printer_queue):
-		if printer_queue.is_empty():
-			print("No more jobs to print!")
-		else:
-			printer_queue.dequeue()
-		
+
+	def get_job(self, print_queue):
+		try:
+			self.current_job = print_queue.dequeue()
+		except IndexError:  # Queue is empty
+			return "No more jobs to print."
+
 	def print_job(self, job):
 		while job.pages > 0:
 			job.print_page()
-			
-		if job.check_complete:
-			print("Job Printed!")
-		
-		
-		
-		
-		
-		
+
+		if job.check_complete():
+			return "Printing complete."
+		else:
+			return "An error occurred."
