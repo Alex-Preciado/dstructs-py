332372fd50e52877fc02bee61ba5e6a159745859
Alex Preciado <alexpreciado1@gmail.com>
2019-05-29 03:00:55 -0400

    Fill and document methods of Deque class

--- dstructs/Deque.py
+++ dstructs/Deque.py
@@ -4,25 +4,80 @@ class Deque:
 		self.items = []
 
 	def add_front(self, item):
-		pass
+		"""
+			Inserts an item into the 0th index of the list representing the Deque.
+			
+			Runtime of this method is linear, or O(n), because every time an item is inserted at the front of the list, all other items are shifted on position to the right.
+		"""
+		self.items.insert(0,item)
 		
 	def add_rear(self, item):
-		pass
+		"""
+			Appends an item at the end of the list representing the Deque.
+			
+			Runtime of this method is O(1).
+		"""
+		self.items.append(item)
 		
 	def remove_front(self):
-		pass
+		"""
+			Removes the first item of the list representing the Deque.
+			
+			Runtime of this method is linear, or O(n), as each item has to be shifted one position to the left when we remove the first item of the list.
+		"""
+		if self.items:
+			return self.items.pop(0)
+		else:
+			return None
 		
 	def remove_rear(self):
-		pass
+		"""
+			Removes the last item of the list representing the Deque.
+			
+			Runtime of this method is O(1) because this only removes the last item leaving the rest intact.
+		"""
+	
+		if self.items:
+			return self.items.pop( )
+		else:
+			return None
 	
 	def peek_front(self):
-		pass
+		"""
+			Returns the first item of the list representing the Deque.
+
+			Runtime of this method is O(1).
+		"""
+		if self.items:
+			return self.items[0]
+		else:
+			return None
 		
 	def peek_rear(self):
-		pass
+		"""
+			Returns the last item of the list representing the Deque.
+
+			Runtime of this method is O(1).
+		"""
+
+		if self.items:
+			return self.items[-1]
+		else:
+			return None
 	
 	def size(self):
-		pass
-	
+		"""
+			Method to return the lenght of the list representing the Deque.
+			
+			Runtime for this method is O(1)
+		"""
+		return len(self.items)
+		
+		
 	def is_empty(self):
-		pass
+		"""
+			Method to check if the Deque's list is empty.
+			
+			Runtime for this method is O(1)
+		"""	
+		return self.items == []
